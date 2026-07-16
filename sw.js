fc87bd7ae0eb2ef3a98623157b5180aeb915093b
lsy9677-eng <lsy9677@gmail.com>
2026-07-16 14:37:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,6 @@
-const CACHE_NAME = 'tenniskj-v10-1';
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
+// v10.2: 자동 업데이트/강제 새로고침을 사용하지 않는 안정화 서비스워커
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
-  event.waitUntil((async () => {
-    const names = await caches.keys();
-    await Promise.all(names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name)));
-    await self.clients.claim();
-  })());
+  event.waitUntil(self.clients.claim());
 });
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
+self.addEventListener('fetch', () => {});
